f94c8111005bd105d228d4381b06554e04c1ffca
mikaelfr <zolppi.sliksu@gmail.com>
2020-06-04 14:44:38 +0300

    Add tags to TaskInstanceModelView and replace hasMatches with has_matches

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -2953,12 +2953,12 @@ class TaskInstanceModelView(TaskTagModelView):
     list_columns = ['state', 'dag_id', 'task_id', 'execution_date', 'operator',
                     'start_date', 'end_date', 'duration', 'job_id', 'hostname',
                     'unixname', 'priority_weight', 'queue', 'queued_dttm', 'try_number',
-                    'pool', 'task_tags', 'log_url']
+                    'pool', 'tags', 'log_url']
 
     order_columns = [item for item in list_columns if item not in ['try_number', 'log_url', 'task_tags']]
 
     search_columns = ['state', 'dag_id', 'task_id', 'execution_date', 'hostname',
-                      'queue', 'pool', 'operator', 'start_date', 'end_date', 'task_tags']
+                      'queue', 'pool', 'operator', 'start_date', 'end_date', 'tags']
 
     base_order = ('job_id', 'asc')
 
@@ -2980,12 +2980,12 @@ class TaskInstanceModelView(TaskTagModelView):
         return None
 
     def tags_formatter(attr):
-        tags = attr.get('task_tags')
+        tags = attr.get('tags')
         tags_str = [tag.name for tag in tags]
         if tags:
             return ', '.join(tags_str)
         else:
-            return ' '
+            return ''
 
     formatters_columns = {
         'log_url': log_url_formatter,
@@ -2998,7 +2998,7 @@ class TaskInstanceModelView(TaskTagModelView):
         'queued_dttm': wwwutils.datetime_f('queued_dttm'),
         'dag_id': wwwutils.dag_link,
         'duration': duration_f,
-        'task_tags': tags_formatter
+        'tags': tags_formatter
     }
 
     @provide_session
