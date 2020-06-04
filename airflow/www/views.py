27cf576507b73e1555eacbedd34977f664cb1806
mikaelfr <miksu.fredriksson@gmail.com>
2020-06-04 15:43:34 +0300

    Pass datamodel instead of model to filter

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -2965,8 +2965,8 @@ class TaskInstanceModelView(AirflowModelView):
     base_filters = [['dag_id', DagFilter, lambda: []]]
 
     column_filters = [
-        TagContainsFilter(column_name='tags', datamodel=models.TaskInstance),
-        TagNotContainsFilter(column_name='tags', datamodel=models.TaskInstance)
+        TagContainsFilter(column_name='tags', datamodel=datamodel),
+        TagNotContainsFilter(column_name='tags', datamodel=datamodel)
     ]
 
     def log_url_formatter(attr):
